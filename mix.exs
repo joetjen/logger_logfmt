10dbf43130f2705a05447a592dafbc7e778009fa
Jan Oetjen <jo@onscreen.net>
2025-12-03 23:23:18 +0100

    fix: update version and source URL in mix.exs
    Updated the version to 1.0.0 and corrected the source URL to reflect the proper repository link.

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
 defmodule LoggerLogfmt.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
-  @source_url "https://github.com/yourorg/logger_logfmt"
+  @version "1.0.0"
+  @source_url "https://github.com/joetjen/logger_logfmt"
 
   def project do
     [
